157ce158e01a0cf213af0bcfa44a9addffed093b
Andre <andreagandino@gmail.com>
2015-08-05 11:57:39 +0200

    Better handling of the multiple variant for the color field.

--- templates/fields/color.php
+++ templates/fields/color.php
@@ -2,17 +2,19 @@
 
 $multiple = $field->config( 'multiple' );
 $size = $field->config( 'size' );
-$field_value = $field->value();
-$field_handle = $field->handle();
+$value = $field->value();
+$handle = $field->handle();
 
 if ( $multiple !== false && is_array( $multiple ) ) {
 
 	foreach ( $multiple as $k => $field_label ) {
-		if ( isset( $field_value[$k] ) ) {
+		$field_value = $value;
+
+		if ( is_array( $field_value ) && isset( $field_value[$k] ) ) {
 			$field_value = $field_value[$k];
 		}
 
-		$field_handle = $field_handle . '[' . $k . ']';
+		$field_handle = $handle . '[' . $k . ']';
 
 		echo '<div class="ev-color-input-wrapper">';
 			echo '<span class="ev-sub-label">' . esc_html( $field_label ) . '</span>';
@@ -22,5 +24,5 @@ if ( $multiple !== false && is_array( $multiple ) ) {
 
 }
 else {
-	echo '<input type="text" class="ev-color-input" size="' . esc_attr( $size ) .'" name="' . esc_attr( $field_handle ) . '" value="' . esc_attr( $field_value ) . '">';
+	echo '<input type="text" class="ev-color-input" size="' . esc_attr( $size ) .'" name="' . esc_attr( $handle ) . '" value="' . esc_attr( $value ) . '">';
 }
