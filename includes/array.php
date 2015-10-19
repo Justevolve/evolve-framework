279d114416f63c08c9869da2cf114f292f416245
Andre <andreagandino@gmail.com>
2015-10-19 14:33:24 +0200

    Added array manipulation helper functions.

--- includes/array.php
+++ includes/array.php
@@ -39,3 +39,204 @@ function ev_array_search_index( $array, $args = array() ) {
 
 	return -1;
 }
+
+/**
+ * Find a specific subkey in a multidimensional array.
+ *
+ * @since 0.4.0
+ * @param array $array The haystack array.
+ * @param string $path The search path.
+ * @param mixed $default Default value when no results are found.
+ * @param string $return Either 'value' or 'ids'.
+ * @return mixed
+ */
+function ev_array_find( $array, $path = '', $default = null, $return = 'value' ) {
+	$value = $default;
+	$element = $array;
+	$ids = array();
+
+	$delimiter = '/';
+	$comparison = ':';
+
+	$path = trim( $path, $delimiter );
+
+	if ( $path === '' ) {
+		return $element;
+	}
+
+	$segments = explode( $delimiter, $path );
+	$path_found = true;
+
+	foreach ( $segments as $segment ) {
+		$found = false;
+		$search = strrpos( $segment, $comparison ) !== false;
+
+		if ( $search ) {
+			list( $sk, $sv ) = explode( $comparison, $segment );
+
+			if ( array_key_exists( $sk, $element ) ) {
+				if ( $sv == $element[$sk] ) {
+					$found = true;
+					$value = $sv;
+					$element = $element[$sk];
+					$ids[] = $sk;
+				}
+			}
+			else {
+				foreach ( $element as $ek => $ev ) {
+					if ( is_array( $ev ) && array_key_exists( $sk, $ev ) ) {
+						if ( $sv == $ev[$sk] ) {
+							$found = true;
+							$value = $ev;
+							$element = $element[$ek];
+							$ids[] = $ek;
+						}
+					}
+				}
+			}
+		}
+		elseif ( array_key_exists( $segment, $element ) ) {
+			$found = true;
+			$value = $element[$segment];
+			$element = $element[$segment];
+			$ids[] = $segment;
+		}
+		else {
+			$found = false;
+		}
+
+		$path_found = $path_found && $found;
+	}
+
+	if ( $return == 'value' ) {
+		if ( $path_found ) {
+			return $value;
+		}
+		else {
+			return $default;
+		}
+	}
+
+	return $ids;
+}
+
+/**
+ * Add a value to a specific subkey in a multidimensional array.
+ *
+ * @since 0.4.0
+ * @param array &$array The haystack array.
+ * @param string $path The search path.
+ * @param mixed $add The value to add.
+ */
+function ev_array_add( &$array, $path = '', $add = null ) {
+	$value = null;
+	$element = $array;
+
+	$delimiter = '/';
+	$comparison = ':';
+
+	$find = find( $array, $path, null, 'ids' );
+
+	if ( ! empty( $find ) ) {
+		$path = trim( $path, $delimiter );
+
+		if ( $path === '' ) {
+			return $element;
+		}
+
+		$path = array_shift( $find );
+
+		if ( is_string( $path ) || is_integer( $path ) && array_key_exists( $path, $array ) ) {
+			add( $array[$path], implode( $delimiter, $find ), $add );
+
+			if ( count( $find ) === 0 ) {
+				$array[$path][] = $add;
+			}
+		}
+	}
+}
+
+/**
+ * Update a value in a specific subkey in a multidimensional array.
+ *
+ * @since 0.4.0
+ * @param array &$array The haystack array.
+ * @param string $path The search path.
+ * @param mixed $replacement The value to replace.
+ */
+function ev_array_update( &$array, $path = '', $replacement = null ) {
+	$value = null;
+	$element = $array;
+
+	$delimiter = '/';
+	$comparison = ':';
+
+	$find = find( $array, $path, null, 'ids' );
+
+	if ( ! empty( $find ) ) {
+		$path = trim( $path, $delimiter );
+
+		if ( $path === '' ) {
+			return $element;
+		}
+
+		$path = array_shift( $find );
+
+		if ( is_string( $path ) || is_integer( $path ) && array_key_exists( $path, $array ) ) {
+			update( $array[$path], implode( $delimiter, $find ), $replacement );
+
+			if ( count( $find ) === 0 ) {
+				$array[$path] = $replacement;
+			}
+		}
+	}
+}
+
+/**
+ * Remove a specific subkey in a multidimensional array.
+ *
+ * @since 0.4.0
+ * @param array &$array The haystack array.
+ * @param string $path The search path.
+ */
+function remove( &$array, $path = '' ) {
+	$value = null;
+	$element = $array;
+
+	$delimiter = '/';
+	$comparison = ':';
+
+	$find = find( $array, $path, null, 'ids' );
+
+	if ( ! empty( $find ) ) {
+		$path = trim( $path, $delimiter );
+
+		if ( $path === '' ) {
+			return $element;
+		}
+
+		$path = array_shift( $find );
+
+		if ( is_string( $path ) || is_integer( $path ) && array_key_exists( $path, $array ) ) {
+			remove( $array[$path], implode( $delimiter, $find ) );
+
+			if ( count( $find ) === 0 ) {
+				unset( $array[$path] );
+			}
+		}
+	}
+}
+
+/**
+ * Check if a specific subkey is set in a multidimensional array.
+ *
+ * @since 0.4.0
+ * @param array $array The haystack array.
+ * @param string $path The search path.
+ * @return boolean
+ */
+function is_set( $array, $path = '' ) {
+	$find = find( $array, $path, null );
+
+	return $find !== null;
+}
