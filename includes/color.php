02c7510ba029a7460741ee4966ea2897567f8a82
Andrea Gandino <andreagandino@gmail.com>
2016-01-25 11:07:07 +0100

    no message

--- includes/color.php
+++ includes/color.php
@@ -247,3 +247,80 @@ function ev_color_presets_modal_load() {
 }
 
 add_action( 'wp_ajax_ev_color_presets_modal_load', 'ev_color_presets_modal_load' );
+
+/**
+ * Add custom colors to the TinyMCE interface.
+ *
+ * @since 0.4.0
+ * @param array $init The TinyMCE initialization array.
+ * @return array
+ */
+function ev_tiny_mce_custom_colors( $init ) {
+	$presets = ev_get_user_color_presets();
+
+	if ( empty( $presets ) ) {
+		return $init;
+	}
+
+  	$default_colours = '"000000", "Black",
+                      "993300", "Burnt orange",
+                      "333300", "Dark olive",
+                      "003300", "Dark green",
+                      "003366", "Dark azure",
+                      "000080", "Navy Blue",
+                      "333399", "Indigo",
+                      "333333", "Very dark gray",
+                      "800000", "Maroon",
+                      "FF6600", "Orange",
+                      "808000", "Olive",
+                      "008000", "Green",
+                      "008080", "Teal",
+                      "0000FF", "Blue",
+                      "666699", "Grayish blue",
+                      "808080", "Gray",
+                      "FF0000", "Red",
+                      "FF9900", "Amber",
+                      "99CC00", "Yellow green",
+                      "339966", "Sea green",
+                      "33CCCC", "Turquoise",
+                      "3366FF", "Royal blue",
+                      "800080", "Purple",
+                      "999999", "Medium gray",
+                      "FF00FF", "Magenta",
+                      "FFCC00", "Gold",
+                      "FFFF00", "Yellow",
+                      "00FF00", "Lime",
+                      "00FFFF", "Aqua",
+                      "00CCFF", "Sky blue",
+                      "993366", "Red violet",
+                      "FFFFFF", "White",
+                      "FF99CC", "Pink",
+                      "FFCC99", "Peach",
+                      "FFFF99", "Light yellow",
+                      "CCFFCC", "Pale green",
+                      "CCFFFF", "Pale cyan",
+                      "99CCFF", "Light sky blue",
+                      "CC99FF", "Plum"';
+
+    $custom_colours = '';
+
+    foreach ( $presets as $preset ) {
+    	$hex = str_replace( '#', '', $preset['hex'] );
+
+    	$custom_colours .= sprintf( '"%s", "%s",', $hex, $preset['label'] );
+    }
+
+    $custom_colours = trim( $custom_colours, ',' );
+
+    $rows = 5 + ( count( $presets ) % 6 ) + 1;
+
+  // build colour grid default+custom colors
+  $init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']';
+
+  // enable 6th row for custom colours in grid
+  $init['textcolor_rows'] = $rows;
+
+  return $init;
+}
+
+add_filter( 'tiny_mce_before_init', 'ev_tiny_mce_custom_colors' );
