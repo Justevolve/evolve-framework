3383ec77c1f94468e64c4a8773f9808081a97d53
Andrea Gandino <andreagandino@gmail.com>
2016-03-29 11:57:38 +0200

    no message

--- includes/system.php
+++ includes/system.php
@@ -429,10 +429,12 @@ function ev_is_login_page() {
 function ev_backup_configuration( $export = array() ) {
 	$export_options = true;
 	$export_mods = true;
+	$export_widgets = true;
 
 	if ( ! empty( $export ) ) {
 		$export_options = isset( $export['options'] ) && $export['options'] == true;
 		$export_mods = isset( $export['mods'] ) && $export['mods'] == true;
+		$export_widgets = isset( $export['widgets'] ) && $export['widgets'] == true;
 	}
 
 	$data = array(
@@ -451,14 +453,18 @@ function ev_backup_configuration( $export = array() ) {
 		$filename .= '-mods';
 	}
 
-	$exp = serialize( $data );
+	if ( $export_widgets ) {
+		$data['widgets'] = wp_get_sidebars_widgets();
+		$filename .= '-widgets';
+	}
+
 	$filename .= '.' . date( 'Y-m-d' ) . '.txt';
 
 	header( 'Content-disposition: attachment; filename=' . $filename );
 	header( 'Content-type: text/plain' );
 
 	ob_start();
-	echo $exp;
+	echo serialize( $exp );
 	ob_end_flush();
 
 	die();
@@ -478,7 +484,8 @@ function ev_restore_configuration( $data = array() ) {
 	}
 
 	$import_options = isset( $data['options'] );
-	$import_mods = isset( $data['mods'] );
+	$import_mods    = isset( $data['mods'] );
+	$import_widgets = isset( $data['widgets'] );
 
 	if ( ! isset( $data['ev'] ) || ! $data['ev'] ) {
 		return;
@@ -492,6 +499,10 @@ function ev_restore_configuration( $data = array() ) {
 	if ( $import_mods ) {
 		$theme = get_option( 'stylesheet' );
 		update_option( "theme_mods_$theme", $data['mods'] );
+	}
+
+	if ( $import_widgets ) {
+		update_option( 'sidebars_widgets', $data['widgets'] );
 	}
 }
 
