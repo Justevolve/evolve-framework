6bfd1ffedf751b1c4e5e89455bfd12d9a1532f98
Andrea Gandino <andreagandino@gmail.com>
2016-07-18 14:26:46 +0200

    no message

--- evolve-framework.php
+++ evolve-framework.php
@@ -23,7 +23,7 @@ if ( ! ( version_compare( $wp_version, '4.4.0' ) >= 0 ) ) {
  * Plugin Name: Evolve Framework
  * Plugin URI: https://github.com/Justevolve/evolve-framework
  * Description: A WordPress development framework.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Evolve
  * Author URI: http://justevolve.it
  * Text Domain: ev_framework
@@ -108,7 +108,7 @@ class Ev_Framework {
 		define( 'EV_FW', true );
 
 		/* Framework version number. */
-		define( 'EV_FRAMEWORK_VERSION', '1.0.0' );
+		define( 'EV_FRAMEWORK_VERSION', '1.0.1' );
 
 		/* Theme folder. */
 		define( 'EV_THEME_FOLDER', trailingslashit( get_template_directory() ) );
